46bc82d3d6b82687b363b5ffe31bc34bc8aa79e5
Yousopp <yousephagueye@gmail.com>
2022-11-15 18:00:19 -0500

    initialized all files

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import coverImage from '../../assets/cover/';
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Who is Youseoh?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          
+        </p>
+      </div>
+    </section>
+  );
+}
 
+export default About;
