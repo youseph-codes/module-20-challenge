700f2f5a9cade43fa390750b6d90c1232720b35b
Yousopp <yousephagueye@gmail.com>
2022-11-15 18:40:43 -0500

    updated about me tab

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import coverImage from '../../assets/cover/IMG_0670.JPG';
 function About() {
   return (
-    <section className="my-5">
-      <h1 id="about">Who is Youseph?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
-        <p>
-          
-        </p>
-      </div>
+    <section>
+      <h1 id="about" className="center">
+        Who is Youseph Gueye?
+      </h1>
+      <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      />
+      <p> Youseph // Software Engineer // Barista </p>
     </section>
   );
 }
