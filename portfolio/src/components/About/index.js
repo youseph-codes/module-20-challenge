e9239df00e4d6f801b728b3ad63ce4e0cfc76e20
Yousopp <yousephagueye@gmail.com>
2022-11-15 18:36:55 -0500

    updated file format

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import coverImage from '../../assets/cover/';
+import coverImage from '../../assets/cover/IMG_0670.JPG';
 function About() {
   return (
     <section className="my-5">
-      <h1 id="about">Who is Youseoh?</h1>
+      <h1 id="about">Who is Youseph?</h1>
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       <div className="my-2">
         <p>
