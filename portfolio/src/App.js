d8bbc0af136d6b0f69ad9e026a19b0d2aa2f4126
Yousopp <yousephagueye@gmail.com>
2022-11-15 19:23:38 -0500

    START OVERgit add .

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,53 +1,23 @@
-import React, { useState } from "react";
-import Nav from "./components/Nav";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import ContactForm from "./components/Contact";
-
-import Footer from "./components/Footer";
-import "./App.css";
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [categories] = useState([
-    {
-      name: "projects",
-      description: "list of projects",
-    },
-    
-    // { name: "Resume", description: "link to my resume page" },
-    
-
-  ]);
-
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  const [contactSelected, setContactSelected] = useState(false);
-
   return (
-    <div>
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      ></Nav>
-      <main>
-        {!contactSelected ? (
-          <>
-            
-            <Projects currentCategory={currentCategory}></Projects>
-            <About></About>
-            
-          </>
-        ) : (
-          <ContactForm></ContactForm>
-        )}
-      </main>
-
-      <Footer></Footer>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
