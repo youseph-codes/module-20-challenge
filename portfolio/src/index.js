c050c33162112695f42617bbd7f79e4d3a9013f4
Yousopp <yousephagueye@gmail.com>
2022-11-15 19:53:42 -0500

    updated index.js +app.js

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import coverImage from "./assets/cover/cover-image.jpg";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
