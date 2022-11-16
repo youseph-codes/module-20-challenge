02b04b7551b47dd7a1882861262ff7f509deffac
Yousopp <yousephagueye@gmail.com>
2022-11-15 21:39:46 -0500

    updated readme and deployed link

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import coverImage from "./assets/cover/cover-image.jpg";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
